c96acb198976f91fdeda9f47417d15876567ddc0
Dina Charki <dina.charki@outlook.fr>
2024-04-12 11:04:07 +0200

    Résolution du bug sur le calcul du prix

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -10,12 +10,16 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        //Le prbl c'est qu'on utilise des entiers, pas le hours, mais Time
+        //le calculer en long
+        long inHour = ticket.getInTime().getTime() / (60*1000);
+        long outHour = ticket.getOutTime().getTime() / (60*1000);
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
-
+        //duration 0,75
+        float duration = outHour - inHour;
+        duration = duration / 60;
+        
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
                 ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
